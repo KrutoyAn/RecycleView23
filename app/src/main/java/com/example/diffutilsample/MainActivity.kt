5f0a9c1c0ec0b9960f5e65ca436ab7ff68c1dad9
Andrei Krutoy <andrei.krutoy1997@gmail.com>
2022-12-19 23:06:49 +0300

    add animation on btn

--- app/src/main/java/com/example/diffutilsample/MainActivity.kt
+++ app/src/main/java/com/example/diffutilsample/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.diffutilsample
 
+
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.diffutilsample.adapter.WordAdapter
@@ -19,14 +22,21 @@ class MainActivity : AppCompatActivity() {
         Word(5, "RecyclerView"),
     )
     private var idCounter = newWordListList.size + 1
+    private lateinit var imgOne : ImageView
+    private lateinit var imgTwo : ImageView
     private lateinit var binding: ActivityMainBinding
     private val wordAdapter by lazy { WordAdapter() }
+    private lateinit var  runnable : Runnable
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        imgOne = binding.imgAnimation1
+        imgTwo = binding.imgAnimation2
         setContentView(binding.root)
         setAdapter()
+
         if (savedInstanceState == null) {
             wordAdapter.setData(newWordListList)
         } else {
@@ -34,22 +44,48 @@ class MainActivity : AppCompatActivity() {
             idCounter = list.size
             wordAdapter.setData(list)
         }
+
+        runnable = Runnable  {
+            imgOne.animate().scaleX(4f).scaleY(4f).alpha(0f).setDuration(650)
+                .withEndAction {
+                    imgOne.scaleX = 0.8f
+                    imgOne.scaleY = 0.8f
+                    imgOne.alpha = 0.8f
+                }
+
+            imgTwo.animate().scaleX(4f).scaleY(4f).alpha(0f).setDuration(1000)
+                .withEndAction {
+                    imgTwo.scaleX = 0.8f
+                    imgTwo.scaleY = 0.8f
+                    imgTwo.alpha = 0.8f
+                }
+        }
+    }
+
+    private fun startPulse() {
+        runnable.run()
     }
 
     private fun setAdapter() {
+
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = wordAdapter
-        binding.buttonAdd.setOnClickListener {
+        binding.button.setOnClickListener {
+            startPulse()
             val item = Word(idCounter, "$idCounter элемент")
             idCounter++
             val listToUpdate = wordAdapter.wordList.toMutableList()
-                .apply { add(item) }
+                .apply { add(item)
+                }
+
             wordAdapter.setData(listToUpdate)
         }
+
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putSerializable(LIST_KEY, wordAdapter.wordList as java.io.Serializable)
         super.onSaveInstanceState(outState)
     }
+
 }
